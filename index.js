78b119f3616e93cd1e058df03e85e879e0ca94e3
Md Ashiqur Rahman Pranto <59225534+ashiqurrahman696@users.noreply.github.com>
2025-11-09 06:51:09 +0600

    connected mongodb to server

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fm0wyio.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 app.get("/", (req, res) => {
     res.send("MovieMaster Pro server is running");
 });
+
+async function run(){
+    try{
+        await client.connect();
+        const db = client.db("movie_db");
+        const moviesCollection = db.collection("movies");
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`MovieMaster Pro server is running on port ${port}`)
